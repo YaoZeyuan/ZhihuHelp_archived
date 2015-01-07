a6b4d2eb1153f924aa872d1de66e321d2d2c5e5c
yaozeyuan <yaozeyuan93@gmail.com>
2015-01-07 23:29:49 +0800

    *    epub生成模块基本完成，还差生成目录功能没做，可以一放

--- zhihuhelp1.6.9/middleCode/epubBuilder/__init__.py
+++ zhihuhelp1.6.9/middleCode/epubBuilder/__init__.py
@@ -5,18 +5,10 @@ u"""
 提供简单的接口
 
 """
-class Ncx                            
-class NavMap                         
-class NavPoint                       
-class PageList                       
-class PageTarget                     
-class NavList                        
-class NavTarget                      
-
-class Opf
 class Metadata():
     def __init__(self):
         self.content = ''
+        self.coverImg= ''
         self.attr    = {}
         self.metadateList = ['title', 'indentifier', 'language', 'creator', 'description', 'rights', 'publisher']
         return 
@@ -25,7 +17,7 @@ class Metadata():
         self.attr['title'] = title 
         return
     
-    def addIdentifier(self, id):
+    def setUID(self, id):
         self.arrt['identifier'] = id
         return
 
@@ -49,8 +41,8 @@ class Metadata():
         self.attr['publisher'] = publisher
         return
 
-    def addCover(self, coversrc):#需要和mainfest同步执行
-        self.attr['cover'] = coversrc
+    def addCoverImg(self, id):#需要和mainfest同步执行
+        self.coverImg += '<meta name="cover" content="{0}" />\n'.format(id)
         return
     
     def getString(self):#返回根据设定数据产生的metadata字符串
@@ -61,53 +53,365 @@ class Metadata():
                     content += "<dc:{0}>{1}</dc:{0}>\n".format(key, self.attr[key])
                 else:
                     content += "<dc:{0} id='{1}'>{1}</dc:{0}>\n".format(key, self.attr[key])
+        content += self.coverImg
         content += "</metadata>\n"
         return content
 
+import shutil
+import zipfile
+import os
 
-class Manifest
-class ManifestItem
-class Spine
-class Guide
+class Book():
+    u'假定制作电子书添加文件时为线性添加'
+    def __init__(self, bookTitle, bookID):
+        self.mainfest    = Mainfest()
+        self.spine       = Spine()
+        self.guide       = Guide() 
+        self.metadata    = Metadata() 
+        self.bookID      = ''
+        self.bookTitle   = bookTitle
+        self.indentifier = 1 #用于生成递增ID
+        self.index       = ''#用于生成目录 
 
-def init(booktitle):
-    mkdir('./' + (str)booktitle)
-    chdir('./' + (str)booktitle)
-    writeMimetype()
+        mkdir('./' + (str)bookTitle)
+        chdir('./' + (str)bookTitle)
+        self.__writeMimetype()
+        self.addTitle(self, bookTitle):
+        self.addIdentifier(self, bookID):
 
-    mkdir('./META-INF')
-    chdir('./META-INF')
-    writeContentXml()
-    chdir('../')
+        mkdir('./META-INF')
+        chdir('./META-INF')
+        self.__writeContentXml()
+        chdir('../')
 
-    mkdir('./OEBPS')
-    mkdir('./OEBPS/html')
-    mkdir('./OEBPS/image')
-    chdir('./OEBPS')
+        mkdir('./OEBPS')
+        mkdir('./OEBPS/html')
+        mkdir('./OEBPS/image')
+        chdir('./OEBPS')
+        return
 
-def writeMimetype():
-    f = fopen('mimetype', 'wb')
-    f.write('application/epub+zip')
-    f.close()
-    return
+    def __writeMimetype(self):
+        f = fopen('mimetype', 'wb')
+        f.write('application/epub+zip')
+        f.close()
+        return
+    
+    def __writeContentXml(self):
+        f = fopen('mimetype', 'wb')
+        f.write(u'''
+                <?xml version="1.0"?>
+                <container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
+                  <rootfiles>
+                    <rootfile full-path="OEBPS/content.opf" 
+                    media-type="application/oebps-package+xml" />
+                  </rootfiles>
+                </container>
+                ''')
+        f.close()
+        return
 
-def writeContentXml():
-    f = fopen('mimetype', 'wb')
-    f.write(u'''
-<?xml version="1.0"?>
-<container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
-  <rootfiles>
-    <rootfile full-path="OEBPS/content.opf" 
-    media-type="application/oebps-package+xml" />
-  </rootfiles>
-</container>
-''')
-    f.close()
-    return
+    def __getFileName(self, src):
+        return os.path.split(src)[1]
 
+    def addHtml(self, src, title, linear=True):
+        u'章节之间使用文件名区隔，而不是使用文件夹形式进行区隔，html文件夹中只有一层目录'
+        shutil.copy(src, './html/')
+        fileName = self.__getFileName(src)
+        self.indentifier += 1 
+        id = self.indentifier
+        self.mainfest.addHtml(fileName, id)
+        self.spine.addFile(id, linear=True)
+        self.ncx.addFile(fileName, id, title)
+        return
+
+    def addImg(self, src):
+        shutil.copy(src, './images/')
+        fileName = self.__getFileName(src)
+        self.indentifier += 1 
+        id = self.indentifier
+        self.mainfest.addImg(fileName, id)
+        return
+
+    def addCss(self, src):
+        shutil.copy(src, './')
+        fileName = self.__getFileName(src)
+        self.indentifier += 1 
+        id = self.indentifier
+        self.mainfest.addCss(fileName, id)
+        return
+
+    def addCoverHtml(self, src, title):
+        u"""
+        cover只能是html网页
+        """
+        shutil.copy(src, './')
+        fileName = self.__getFileName(src)
+        self.indentifier += 1 
+        id = self.indentifier
+        self.mainfest.addHtml(fileName, id)
+        self.spine.addFile(id, linear=False)
+        self.ncx.addFile(fileName, id, title)
+        self.guide.addCoverHtml(fileName, title)
+        return
+
+    def addInfoPage(self, src, title):
+        u"""
+        cover只能是html网页
+        """
+        shutil.copy(src, './')
+        fileName = self.__getFileName(src)
+        self.indentifier += 1 
+        id = self.indentifier
+        self.mainfest.addHtml(fileName, id)
+        self.spine.addFile(id, linear=True)
+        self.ncx.addFile(fileName, id, title)
+        self.guide.addInfoPage(fileName, title)
+        return
+
+    def addCoverImg(self, src):
+        shutil.copy(src, './images/')
+        fileName = self.__getFileName(src)
+        self.indentifier += 1 
+        id = self.indentifier
+        self.mainfest.addImg(fileName, id)
+        self.metaData.addCoverImg(id)
+        return
+
+    def createChapter(self, src, id, title):
+        shutil.copy(src, './html')
+        fileName = self.__getFileName(src)
+        self.indentifier += 1 
+        id = self.indentifier
+        self.ncx.addChapter(fileName, id, title)
+        return
+
+    def endChapter(self):
+        self.ncx.endChapter()
+        return
+
+    def addTitle(self, title):
+        self.metaData.addTitle(title)
+        self.ncx.addBookTitle(title)
+        return
+    
+    def addIdentifier(self, id):
+        self.bookID = id
+        self.metaData.setUID(id)
+        self.ncx.setUID(id)
+        return
+
+    def addLanguage(self, lang):
+        self.metaData.addLanguage(lang)
+        return
+    
+    def addCreator(self, creator):
+        self.metaData.addCreator(creator)
+        return
+
+    def addDesc(self, desc):
+        self.metaData.addDesc(desc)
+        return
+
+    def addRight(self, right):
+        self.metaData.addRight(right)
+        return
+
+    def addPublisher(self, publisher):
+        self.metaData.addPublisher(publisher)
+        return
+
+    def createIndex(self):
+        return
+    
+    def buildingEpub(self):
+        opf = open('./OEBPS/content.opf', 'w')
+        opf.write(u"""
+                    <?xml version='1.0' encoding='utf-8'?>
+                      <package xmlns="http://www.idpf.org/2007/opf" 
+                        xmlns:dc="http://purl.org/dc/elements/1.1/" 
+                        unique-identifier="{0}" version="2.0">
+                        {1}
+                        {2}
+                        {3}
+                        {4}
+                    </package>
+                  """.format(self.bookID, self.metaData.getString(), self.manifest.getString(), self.spine.getString(), self.guide.getString()))
+        opf.close()
+        ncx = open('./OEBPS/toc.ncx', 'w')
+        ncx.write(self.ncx.getString())
+        ncx.close()
+        #应当再加上生成目录的功能
+        
+        #直接使用的旧版函数，应当予以更新
+        epub = zipfile.ZipFile(os.path.abspath('../../' + os.curdir +'知乎回答集锦/' + bookTitle), 'w', zipfile. ZIP_STORED, allowZip64=True)
+        epub.write('mimetype')
+        def Help_ZipToEpub(Dir='.'):
+            for p in os.listdir(Dir):
+                if p == bookTitle or p == 'mimetype':
+                    print u'该文件已添加，自动跳过'
+                    continue
+                filepath = os.path.join(Dir,p)
+                if  not os.path.isfile(filepath):
+                    if  p == '.' or p == '..':
+                        continue
+                    Help_ZipToEpub(Dir=filepath)
+                else:
+                    print u'将{}添加至电子书内'.format(filepath)
+                    epub.write(filepath, compress_type=zipfile.ZIP_STORED)
+        Help_ZipToEpub()
+        epub.close()
+        return
+
+class Mainfest():
+    u'规定，所有img都应放在在images文件夹中，所有html内容都应放在html中'
+    def __init__(self):
+        self.img  = u''
+        self.html = u''
+        self.css  = u''
+        self.imgType = {'jepg' : 'jpg', 'png' : 'png', 'svg' : 'svg', 'gif' : 'gif'}
+        return
+
+    def addImg(fileName='', id=1):
+        u"图像文件只能是png,jpg,gif和svg四种类型"
+        fileExt  =  os.path.splitText(fileName)[1][1:]
+        fileType =  'image/' + self.imgType.get((fileExt[1:]).lower(), 'png')
+        self.img += u"""<item id='{0}' href='{1}' media-type="{2}"/>\n""".format((str)id, 'images/' + fileName, fileType)
+        return
+
+    def addHtml(fileName='', id=1):
+        self.html += u"""<item id='{0}' href='{1}' media-type="application/xhtml+xml"/>\n""".format((str)id, 'html/' + fileName)
+        return
+
+    def addCss(fileName='', id=1):
+        self.css  += u"""<item id="{0}" href="{1}" media-type="text/css"/>\n""".format((str)id, fileName)
+        return
+    
+    def getString(self):
+        content = u"""
+          <manifest>
+          {0}
+          {1}
+          {2}
+          </manifest>
+        """.format(self.img, self.html, self.css)
+        return
+
+class Spine():
+    def __init__(self):
+        self.spine = u''
+        return
+
+    def addFile(self, id=1, linear=True):
+        if linear:
+            linear = 'yes'
+        else:
+            linear = 'no'
+        self.spine += u"""<itemref idref="{0}" linear="{1}"/>\n""".format((str)id, linear)
+        return
+    
+    def getString(self):
+        content = u"""
+        <spine toc="ncx">
+          {0}
+        </spine>
+        """.format(self.spine)
+        return content
+
+class Guide():
+    def __init__(self):
+        self.guide = u""
+        return
+
+    def addCoverHtml(self, fileName, title):
+        self.guide += u"""<reference href="{0}" type="cover" title="{1}"/>\n""".format(fileName, title) 
+        return
+
+    def addInfoPage(self, fileName, title):
+        self.guide += u"""<reference href="{0}" type="title-page" title="{1}"/>\n""".format(fileName, title) 
+        return
+
+    def addIndex(self, fileName, title):
+        self.guide += u"""<reference href="{0}" type="toc" title="{1}"/>\n""".format(fileName, title) 
+        return
+
+    def getString(self):
+        content = u"""
+        <guide>
+          {0}
+        </guide>
+        """.format(self.guide)
+        self.guide
+        return
+
+class Ncx():
+    def __init__(self):
+        self.bookTitle = ''
+        self.metaData  = ''
+        self.depth     = -1
+        self.ncx       = ''
+        return
+
+    def addFile(self, fileName, id, title):
+        self.ncx += u"""
+        <navPoint id="{0}" playOrder="{0}">
+          <navLabel>
+             <text>{1}</text>
+          </navLabel>
+          <content src="html/{2}"/>
+        </navPoint>
+        """.format((str)id, title, fileName)
+        return
+
+    def addChapter(self, fileName, id, title):
+        self.ncx += u"""
+        <navPoint id="{0}" playOrder="{0}">
+          <navLabel>
+             <text>{1}</text>
+          </navLabel>
+          <content src="html/{2}"/>
+        """.format((str)id, title, fileName)
+        return 
+
+    def endChapter(self):
+        self.ncx += u"""</navPoint>\n"""
+        return
+
+    def addBookTitle(self, title):
+        self.bookTitle += u"""
+        <docTitle>
+           <text>{0}</text>
+        </docTitle>
+        """.format(title)
+        return
+
+    def setUID(self, uid):
+        self.metaData += u"""
+        <head>
+          <meta name="dtb:uid" content="{0}"/>
+          <meta name="dtb:depth" content="-1"/>
+          <meta name="dtb:totalPageCount" content="0"/>
+          <meta name="dtb:maxPageNumber" content="0"/>
+        </head>
+        """.format((str)uid)
+        return
+    
+    def getString(self):
+        content = u"""
+        <?xml version='1.0' encoding='utf-8'?>
+        <!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN" 
+          "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
+        <ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">
+        {0}
+        {1}
+        <navMap>
+        {2}
+        </navMap>
+        """.format(self.metaData, self.bookTitle, self.ncx)
+        return content
 
 #工具函数
 import os
+import re
 def mkdir(path):
     try:
         os.mkdir(path)
