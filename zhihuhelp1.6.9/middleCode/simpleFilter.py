6d9c63bf1dfe54ad3c73f59f46d1388cf2ba6e95
yaozeyuan <yaozeyuan93@gmail.com>
2015-02-02 00:20:13 +0800

    *       添加了图片处理函数，但没有经过测试，需要再修改

--- zhihuhelp1.6.9/middleCode/simpleFilter.py
+++ zhihuhelp1.6.9/middleCode/simpleFilter.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import datetime
+import re
 class baseFilter():
     '''
     先查出来所有的答案数据
@@ -8,6 +9,8 @@ class baseFilter():
     制成html文件使用epubBuilder生成电子书
     '''
     def __init__(self, cursor = None, kind = {}):
+        self.imgSet = set()
+        self.imgBasePath = '../image/'
         return
     def getChapterFrontPage(self):
         infoDict = {
@@ -17,6 +20,75 @@ class baseFilter():
                 'GuideImg' : '',
                 }
         return infoDict
+    
+    def authorLogoFix(self, imgHref = ''):
+        self.imgSet.add(imgHref)
+        return self.imgBasePath + self.getFileName(imgHref)
+
+    def answerImgFix(self, answerContent = '', imgQuarty = 1):
+        #只能用在Question/Answer的直接提取中，在这里面src不能直接使用，需要用data-actualsrc属性来辅助完成
+        #if imgQuarty == 0:
+        #    answerContent = self.removeTag(answerContent, ['img'])
+        #else:
+        #    if imgQuarty == 1:
+        #        for imgTag in re.findall(r'<img.*?>', text):
+        #            answerContent = answerContent.replace(imgTag, self.fixPic(self.removeTagAttribute(imgTag, ['data-original']).replace('data-rawheight', 'height')[:-1] + u' alt="知乎图片"/>'))
+        #    else:
+        #        for imgTag in re.findall(r'<img.*?>', text):
+        #            try :
+        #                imgTag.index('data-original')
+        #            except  ValueError:
+        #                #所考虑的这种情况存在吗？存疑
+        #                answerContent = answerContent.replace(imgTag, self.fixPic(imgTag.replace('data-rawheight', 'height')[:-1] + u' alt="知乎图片"/>'))
+        #            else:
+        #                #将data-original替换为src即为原图
+        #                answerContent = answerContent.replace(imgTag, self.fixPic(self.removeTagAttribute(imgTag, ['src']).replace('data-original', 'src').replace("data-rawheight", 'height')[:-1] + u' alt="知乎图片"/>'))
+
+        if imgQuarty == 0:
+            answerContent = self.removeTag(answerContent, ['img', 'noscript'])
+        else:
+            answerContent = self.removeTag(answerContent, ['noscript'])
+            if imgQuarty == 1:
+                for imgTag in re.findall(r'<img.*?>', text):
+                    imgContent = self.removeTagAttribute(imgTag, ['data-original']).replace('data-actualsrc', 'src').replace('data-rawheight', 'height')[:-1] + u' alt="知乎图片"/>'
+                    answerContent = answerContent.replace(imgTag, self.fixPic(imgContent))
+            else:
+                for imgTag in re.findall(r'<img.*?>', text):
+                    try :
+                        imgTag.index('data-original')
+                    except  ValueError:
+                        #所考虑的这种情况存在吗？存疑
+                        answerContent = answerContent.replace(imgTag, self.fixPic(imgTag.replace('data-actualsrc', 'src').replace('data-rawheight', 'height')[:-1] + u' alt="知乎图片"/>'))
+                    else:
+                        #将data-original替换为src即为原图
+                        answerContent = answerContent.replace(imgTag, self.fixPic(self.removeTagAttribute(imgTag, ['src']).replace('data-original', 'src').replace("data-rawheight", 'height')[:-1] + u' alt="知乎图片"/>'))
+        
+        return answerContent
+    
+    def fixNoScriptTag(self, content = ''):
+        
+        return
+
+    def fixPic(self, imgTagContent = ''):
+        for src in re.findall(r'(?<=src=")http://[/\w\.^"]*?zhimg.com[/\w^"]*?.jpg', imgTagContent):
+            imgTagContent = imgTagContent.replace(src, self.imgBasePath + self.getFileName(src))
+            self.imgSet.add(src)
+        return '<div class="duokan-image-single">{}</div>'.format(imgTagContent)
+
+    def removeTagAttribute(self, tagContent = '', removeAttrList = []):
+        for attr in removeAttrList:
+            for attrStr in re.findall(r'\s' + attr + '[^\s>]*', tagContent):
+                tagContent = tagContent.replace(attrStr, '')
+        return tagContent
+    
+    def removeTag(text='', tagname=[]):
+        for tag in tagname:
+            text = text.replace('</'+tag+'>', '')
+            text = re.sub(r"<" + tag + r'.*?>', '', text)
+        return text
+
+    def getFileName(self, imgHref = ''):
+        return imgHref.split('/')[-1]
 
 class questionFilter(baseFilter):
     u'每运行一次filter就相当于生成了一本电子书，所以在这个里面也应当为之加上封面，最后输出时大不了再跳过封面输出就好了，电子书应当每个章节都有自己的封面，同时也要有一个总封面'
