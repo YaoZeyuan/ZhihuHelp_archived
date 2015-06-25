d579776297a419dd0c9eadbdfb6aa58967c72ce5
姚泽源 <yaozeyuan93@gmail.com>
2015-06-26 00:14:48 +0800

    添加针对话题的单元测试

---
+++ zhihuhelp1.7.0/unit/contentParseTest/correct_topic_questionInfo.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+__author__ = 'yao'
+
+from base_test_function import *
+
+checkList = [
+    {
+        "questionIDinQuestionDesc": "20840874",
+        "questionTitle": "哪些东西买了之后，会让人因生活质量和幸福感提升而感觉相见恨晚？"
+    },
+    {
+        "questionIDinQuestionDesc": "26753619",
+        "questionTitle": "如何成为一个优秀的男朋友？"
+    },
+    {
+        "questionIDinQuestionDesc": "20687290",
+        "questionTitle": "没有锻炼基础的人，如何增肌与减脂？"
+    },
+    {
+        "questionIDinQuestionDesc": "20894671",
+        "questionTitle": "日常生活中有哪些十分钟就能学会并可以终生受用的技能？"
+    },
+    {
+        "questionIDinQuestionDesc": "23648378",
+        "questionTitle": "被蟒蛇盯上了，如何保命？"
+    },
+    {
+        "questionIDinQuestionDesc": "27910572",
+        "questionTitle": "朋友圈里常发什么样的文字图片的人比较讨厌？"
+    },
+    {
+        "questionIDinQuestionDesc": "26802920",
+        "questionTitle": "为什么有些人不喜欢出身底层的人？"
+    },
+    {
+        "questionIDinQuestionDesc": "22467582",
+        "questionTitle": "有哪些很重要又被忽视的烧菜技巧？"
+    },
+    {
+        "questionIDinQuestionDesc": "22464261",
+        "questionTitle": "租个女友回家过年靠谱吗？有什么风险？"
+    },
+    {
+        "questionIDinQuestionDesc": "23684594",
+        "questionTitle": "得知越来越多的女大学生被包养，已有的价值观开始动摇怎么办？"
+    },
+    {
+        "questionIDinQuestionDesc": "20322809",
+        "questionTitle": "狐臭是怎么产生的？"
+    },
+    {
+        "questionIDinQuestionDesc": "28094932",
+        "questionTitle": "人到底可以有多坏？"
+    },
+    {
+        "questionIDinQuestionDesc": "25485795",
+        "questionTitle": "男生长得难看怎么办？"
+    },
+    {
+        "questionIDinQuestionDesc": "26927514",
+        "questionTitle": "月生活费 800 的女大学生如何在保证三餐不受影响的情况下提升自己的着装搭配和品味？"
+    },
+    {
+        "questionIDinQuestionDesc": "23151702",
+        "questionTitle": "男女交往最忌讳什么？"
+    },
+    {
+        "questionIDinQuestionDesc": "26811466",
+        "questionTitle": "有哪些生活小习惯，慢慢地可以改变一个人的性格或者生活？"
+    },
+    {
+        "questionIDinQuestionDesc": "28070036",
+        "questionTitle": "哪些书可以提高人的情商、判断力和谈话技巧？"
+    },
+    {
+        "questionIDinQuestionDesc": "23151702",
+        "questionTitle": "男女交往最忌讳什么？"
+    },
+    {
+        "questionIDinQuestionDesc": "25148298",
+        "questionTitle": "那些在北上广深国外打拼的年轻人们，你们的父母怎么办？"
+    },
+    {
+        "questionIDinQuestionDesc": "26702926",
+        "questionTitle": "淘宝上买衣服，怎么买出质量，穿出品位？"
+    }
+]
