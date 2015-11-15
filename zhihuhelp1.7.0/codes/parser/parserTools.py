fed817cdd772346fc0d56e7b5dfa5270747cc234
姚泽源 <yaozeyuan93@gmail.com>
2015-11-15 21:44:12 +0800

    *   添加collection_parser和topic_parser

--- zhihuhelp1.7.0/codes/parser/parserTools.py
+++ zhihuhelp1.7.0/codes/parser/parserTools.py
@@ -42,6 +42,14 @@ class ParserTools(BaseClass):
         return ParserTools.match_content("(?<=answer/)\d{8}", rawLink)
 
     @staticmethod
+    def match_topic_id(rawLink):
+        return ParserTools.match_content("(?<=topic/)\d+", rawLink)
+
+    @staticmethod
+    def match_collection_id(rawLink):
+        return ParserTools.match_content("(?<=collection/)\d+", rawLink)
+
+    @staticmethod
     def match_author_id(rawLink):
         return ParserTools.match_content("""(?<=people/)[^/'"]+""", rawLink)
 
@@ -604,11 +612,120 @@ class AuthorInfo(ParserTools):
 
 
 class TopicInfo(ParserTools):
+    def __init__(self, dom=None):
+        self.set_dom(dom)
+        return
+
+    def set_dom(self, dom):
+        self.info = {}
+        if dom:
+            self.dom = dom
+        return
+
+    def get_info(self):
+        self.parse_info()
+        return self.info
+
+    def parse_info(self):
+        self.parse_title()
+        self.parse_topic_id()
+        self.parse_logo()
+        self.parse_follower()
+        self.parse_desc()
+        return self.info
+
+    def parse_title(self):
+        title = self.dom.select('#zh-topic-title h1.zm-editable-content')
+        if not title:
+            BaseClass.logger.debug(u'话题标题未找到')
+        self.info['title'] = title[0].get_text()
+        return
+
+    def parse_topic_id(self):
+        topic = self.dom.select('link[rel="canonical"]')
+        if not topic:
+            BaseClass.logger.debug(u'话题id未找到')
+        href = self.get_attr(topic[0], 'href')
+        self.info['topic_id'] = self.match_topic_id(href)
+        return
+
+    def parse_logo(self):
+        logo = self.dom.select('img.zm-avatar-editor-preview')
+        if not logo:
+            BaseClass.logger.debug(u'话题图标未找到')
+        self.info['logo'] = self.get_attr(logo[0], 'src')
+        return
+
+    def parse_follower(self):
+        follower = self.dom.select('div.zm-topic-side-followers-info a strong')
+        if not follower:
+            BaseClass.logger.debug(u'话题关注人数未找到')
+        self.info['follower'] = follower[0].get_text()
+        return
+
+    def parse_desc(self):
+        desc = self.dom.select('#zh-topic-desc div.zm-editable-content')
+        if not desc:
+            BaseClass.logger.debug(u'话题描述未找到')
+        self.info['desc'] = self.get_tag_content(desc[0])
+        return
+
+
+
 
 class CollectionInfo(ParserTools):
+    u"""
+    只解析收藏夹相关信息，暂时不解析创建者相关的信息
+    """
+    def __init__(self, dom=None):
+        self.set_dom(dom)
+        return
 
-class TableInfo(ParserTools):
+    def set_dom(self, dom):
+        self.info = {}
+        if dom:
+            self.dom = dom
+        return
 
+    def get_info(self):
+        self.parse_info()
+        return self.info
+
+    def parse_info(self):
+        self.parse_title()
+        self.parse_collection_id()
+        self.parse_follower()
+        self.parse_desc()
+        return self.info
+
+    def parse_title(self):
+        title = self.dom.select('h2#zh-fav-head-title')
+        if not title:
+            BaseClass.logger.debug(u'收藏夹标题未找到')
+        self.info['title'] = title[0].get_text()
+        return
+
+    def parse_collection_id(self):
+        topic = self.dom.select('meta[http-equiv="mobile-agent"]')
+        if not topic:
+            BaseClass.logger.debug(u'话题id未找到')
+        href = self.get_attr(topic[0], 'content')
+        self.info['collection_id'] = self.match_collection_id(href)
+        return
+
+    def parse_follower(self):
+        follower = self.dom.select('div.zm-side-section div.zm-side-section-inner div.zg-gray-normal a[href*="followers"]')
+        if not follower:
+            BaseClass.logger.debug(u'话题关注人数未找到')
+        self.info['follower'] = follower[0].get_text()
+        return
+
+    def parse_desc(self):
+        desc = self.dom.select('#zh-fav-head-description-source')
+        if not desc:
+            BaseClass.logger.debug(u'话题描述未找到')
+        self.info['desc'] = self.get_tag_content(desc[0])
+        return
 
 class BaseParser(ParserTools):
     def __init__(self, content):
