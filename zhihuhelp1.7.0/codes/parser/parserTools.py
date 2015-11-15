d2a6f1b0f52e2454b8aa7c4f2486bfcb98d040a2
姚泽源 <yaozeyuan93@gmail.com>
2015-11-15 17:01:43 +0800

    *   添加author_parser

--- zhihuhelp1.7.0/codes/parser/parserTools.py
+++ zhihuhelp1.7.0/codes/parser/parserTools.py
@@ -82,7 +82,7 @@ class Author(ParserTools):
 
     def set_dom(self, dom):
         self.info = {}
-        if not (dom is None):
+        if dom:
             self.dom = dom.find('div', class_='zm-item-answer-author-info')
         return
 
@@ -133,7 +133,11 @@ class Author(ParserTools):
         return
 
     def parse_author_name(self):
-        self.info['author_name'] = self.dom.find('a')[-1].text
+        name = self.dom.find('a', class_='author-link')
+        if not name:
+            BaseClass.logger.debug(u'用户名未找到')
+            return
+        self.info['author_name'] = name.text
         return
 
 
@@ -250,7 +254,7 @@ class Answer(ParserTools):
 
     def set_dom(self, dom):
         self.info = {}
-        if not (dom is None):
+        if dom:
             self.header = dom.find('div', class_='zm-item-vote-info')
             self.body = dom.find('div', class_='zm-editable-content')
             self.footer = dom.find('div', class_='zm-meta-panel')
@@ -283,7 +287,7 @@ class Answer(ParserTools):
         return
 
     def parse_answer_content(self):
-        self.info['content'] = self.body.text()
+        self.info['content'] = self.get_tag_content(self.body)
         return
 
     def parse_date_info(self):
@@ -294,17 +298,17 @@ class Answer(ParserTools):
             return
 
         if commit_date:
-            update_date = data_block.text()
+            update_date = data_block.get_text()
             self.info['edit_date'] = self.parse_date(update_date)
             self.info['commit_date'] = self.parse_date(commit_date)
         else:
-            commit_date = data_block.text()
+            commit_date = data_block.get_text()
             self.info['edit_date'] = self.info['commit_date'] = self.parse_date(commit_date)
 
     def parse_comment_count(self):
-        comment = self.footer.find('a', name='addcomment')
+        comment = self.footer.select("a[name='addcomment']")
         if comment:
-            self.info['comment'] = self.match_int(comment.text())
+            self.info['comment'] = self.match_int(comment[0].get_text())
         else:
             BaseClass.logger.debug(u'评论数未找到')
         return
@@ -312,7 +316,7 @@ class Answer(ParserTools):
     def parse_no_record_flag(self):
         no_record_flag = self.footer.find('a', class_='copyright')
         if no_record_flag:
-            self.info['noRecordFlag'] = int(u'禁止转载' in no_record_flag.text())
+            self.info['noRecordFlag'] = int(u'禁止转载' in no_record_flag.get_text())
         else:
             BaseClass.logger.debug(u'禁止转载标志未找到')
         return
@@ -345,7 +349,7 @@ class QuestionInfo(ParserTools):
 
     def set_dom(self, dom):
         self.info = {}
-        if not (dom is None):
+        if dom:
             self.dom = dom
             self.side_dom = dom.find('div', _class='zu-main-sidebar')
         return
@@ -420,6 +424,184 @@ class QuestionInfo(ParserTools):
 
 
 class AuthorInfo(ParserTools):
+    u"""
+    使用详情页面进行解析
+    """
+    def __init__(self, dom=None):
+        self.set_dom(dom)
+        return
+
+    def set_dom(self, dom):
+        self.info = {}
+        if dom:
+            self.dom = dom
+            self.header_dom = dom.find('div', class_='zm-profile-header')
+            self.detail_dom = dom.find('div', class_='zm-profile-details-wrap')
+            self.side_dom = dom.find('div', class_='zu-main-sidebar')
+        return
+
+    def get_info(self):
+        self.parse_info()
+        return self.info
+
+    def parse_info(self):
+        self.parse_base_info()
+        self.parse_detail_info()
+        self.parse_extra_info()
+        return self.info
+
+    def parse_base_info(self):
+        self.parse_author_id()
+        self.parse_author_hash()
+        self.parse_name()
+        self.parse_sign()
+        self.parse_logo()
+        self.parse_desc()
+        self.parse_weibo()
+        self.parse_gender()
+        return
+
+    def parse_name(self):
+        name = self.dom.select('div.title-section span.name')
+        if not name:
+            BaseClass.logger.debug(u'用户名未找到')
+            return
+        self.info['name'] = name.get_text()
+        return
+
+    def parse_weibo(self):
+        weibo = self.header_dom.select('a.zm-profile-header-user-weibo')
+        if not weibo:
+            BaseClass.logger.debug(u'用户微博未找到')
+            return
+        self.info['weibo'] = self.get_attr(weibo[0], 'href')
+        return
+
+    def parse_sign(self):
+        sign = self.dom.select('div.title-section span.title')
+        if not sign:
+            BaseClass.logger.debug(u'用户签名未找到')
+            return
+        self.info['sign'] = self.get_attr(sign[0], 'title')
+        return
+
+    def parse_logo(self):
+        logo = self.header_dom.select('div.zm-profile-header-avatar-container img.avatar')
+        if not logo:
+            BaseClass.logger.debug(u'用户头像未找到')
+            return
+        self.info['logo'] = self.get_attr(logo[0], 'src')
+        return
+
+    def parse_gender(self):
+        gender = self.header_dom.select('span.edit-wrap input:checked')
+        if not gender:
+            BaseClass.logger.debug(u'用户性别未找到')
+            return
+        self.info['gender'] = self.get_attr(gender, 'class')
+        return
+
+    def parse_author_id(self):
+        item = self.header_dom.select('div.profile-navbar a.item')
+        if not item:
+            BaseClass.logger.debug(u'用户id未找到')
+            return
+        href = self.get_attr(item[0], 'href')
+        self.info['id'] = self.match_author_id(href)
+        return
+
+    def parse_author_hash(self):
+        hash = self.dom.select('script[data-name="current_people"]')
+        if not hash:
+            BaseClass.logger.debug(u'用户hash未找到')
+            return
+        hash = hash[0].get_text().split(',')[-1] #取出hash所在字符串
+        self.info['hash'] =hash.split('"')[1] # 取出hash值
+        return
+
+    def parse_profile_count(self):
+        def parse_items(root = None, kind = 'asks'):
+            node = root.select('a[href*="{} span.num"]'.format(kind))
+            if not node :
+                BaseClass.logger.debug(u'{}未找到'.format(kind))
+            self.info[kind] = node[0].get_text()
+            return
+
+        item_list = [ 'asks', 'answers', 'posts', 'collections', 'logs']
+        div = self.header_dom.select('div.profile-navbar')
+        if not div :
+            BaseClass.logger.debug(u'用户提问-回答-专栏数未找到')
+            return
+        for item in item_list:
+            parse_items(div[0], item)
+        return
+
+    def parse_detail_info(self):
+        detail_items = ['agree', 'thanks', 'collected', 'shared']
+        detail = self.detail_dom.select('.zm-profile-module-desc span strong')
+        if not detail:
+            BaseClass.logger.debug(u'用户赞同-感谢-被收藏数未找到')
+            return
+        for i in len(detail):
+            self.info[detail_items[i]] = detail[i]
+        return
+
+    def parse_desc(self):
+        desc = self.header_dom.select('.description span.content')
+        if not desc :
+            BaseClass.logger.debug(u'用户详情未找到')
+            return
+        self.info['desc'] = self.get_tag_content(desc[0])
+        return
+
+    def parse_extra_info(self):
+        self.parse_followee()
+        self.parse_follower()
+        self.parse_followed_column()
+        self.parse_followed_topic()
+        self.parser_views()
+        return
+
+    def parse_followee(self):
+        followee = self.side_dom.select('div.zm-profile-side-following a[href*="followees"] strong')
+        if not followee:
+            BaseClass.logger.debug(u'用户关注数未找到')
+            return
+        self.info['followee'] = followee.get_text()
+        return
+
+    def parse_follower(self):
+        follower = self.side_dom.select('div.zm-profile-side-following a[href*="followers"] strong')
+        if not follower:
+            BaseClass.logger.debug(u'用户粉丝数未找到')
+            return
+        self.info['follower'] = follower.get_text()
+        return
+
+    def parse_followed_column(self):
+        column = self.side_dom.select('.zm-profile-side-section-title a[href*="columns"] strong')
+        if not column:
+            BaseClass.logger.debug(u'用户关注专栏数未找到')
+            return
+        self.info['followed_column'] = column.get_text()
+        return
+
+    def parse_followed_topic(self):
+        topic = self.side_dom.select('.zm-profile-side-section-title a[href*="topics"] strong')
+        if not topic:
+            BaseClass.logger.debug(u'用户关注话题数未找到')
+            return
+        self.info['followed_topic'] = topic.get_text()
+        return
+
+    def parser_views(self):
+        views = self.side_dom.select('.zm-profile-side-section .zm-side-section-inner span.zg-gray-normal strong')
+        if not views:
+            BaseClass.logger.debug(u'用户被浏览数未找到')
+            return
+        self.info['viewed'] = views.get_text()
+        return
+
 
 class TopicInfo(ParserTools):
 
