8461cd6b49b9b3db729c66831e83b24a0395947b
姚泽源 <yaozeyuan93@gmail.com>
2016-10-29 22:26:00 +0800

    fix : 调整库导入方式

--- zhihuHelp.py
+++ zhihuHelp.py
@@ -4,8 +4,8 @@ import sys  # 修改默认编码
 import os  # 添加系统路径
 
 base_path = unicode(os.path.abspath('.').decode(sys.stdout.encoding))
-sys.path.append(base_path + u'/src/lib')  # 添加基础库路径
-sys.path.append(base_path + u'/src/lib/oauth')  # zhihu oauth 类需要作为默认类导入，否则无法运行 - -
+sys.path.insert(0, base_path + u'/src/lib')  # 添加基础库路径 使用insert方式，确保优先启用项目自带源码包
+sys.path.insert(0, base_path + u'/src/lib/oauth')  # zhihu oauth 类需要作为默认类导入，否则无法运行 - -
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
