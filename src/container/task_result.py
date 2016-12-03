2050ba082d5ff2a3e57cebbebe3667973d2d8d0d
姚泽源 <yaozeyuan93@gmail.com>
2016-12-03 21:02:10 +0800

    feat : 目标：今晚完成！

--- src/container/task_result.py
+++ src/container/task_result.py
@@ -59,8 +59,16 @@ class Article(object):
     u"""
     文章容器
     """
-
-    def __init__(self, data={}):
+    def __init__(self, data):
+        self.article_id = data['article_id']
+        self.title = data['title']
+        self.updated = data['updated']
+        self.created = data['created']
+        self.voteup_count = data['voteup_count']
+        self.column_id = data['column_id']
+        self.content = data['content']
+        self.comment_count = data['comment_count']
+        self.author_id = data['author_id']
         return
 
 
@@ -91,5 +99,14 @@ class TaskResult(object):
         self.task_type = task.task_type
         self.task = task
         self.question_list = []
+        self.article_list = []
         self.info_page = {}
+        return
+
+    def __extract_data(self):
+        u"""
+        根据task类型，从数据库中抽取数据
+        :return:
+        """
+
         return
