efefe8de6ce832c412f975223269a6ad6ac8b295
姚泽源 <yaozeyuan93@gmail.com>
2017-01-14 14:26:06 +0800

    remove : 删除无用的代码

--- src/lib/zhihu_parser/author.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from src.lib.zhihu_parser.base import BaseParser
-from src.lib.zhihu_parser.info.author import AuthorInfo
-
-
-class AuthorParser(BaseParser):
-    def get_extra_info(self):
-        author = AuthorInfo(self.dom)
-        return author.get_info()
