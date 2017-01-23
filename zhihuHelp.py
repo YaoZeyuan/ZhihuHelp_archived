18c16773057a65a49fbf90143f9c8334fc06a316
姚泽源 <yaozeyuan93@gmail.com>
2017-01-23 10:47:02 +0800

    feat : 添加异常捕获功能，方便反馈bug

--- zhihuHelp.py
+++ zhihuHelp.py
@@ -2,6 +2,7 @@
 # 放置于首位
 import sys  # 修改默认编码
 import os  # 添加系统路径
+import traceback
 
 base_path = unicode(os.path.abspath('.').decode(sys.stdout.encoding))
 sys.path.insert(0, base_path + u'/src/lib')  # 添加基础库路径 使用insert方式，确保优先启用项目自带源码包
@@ -13,5 +14,11 @@ sys.setdefaultencoding('utf-8')
 #  执行主程序
 from src.main import ZhihuHelp
 
-helper = ZhihuHelp()
-helper.start()
+try:
+    helper = ZhihuHelp()
+    helper.start()
+except Exception:
+    traceback.print_exc()
+    print u"助手发生异常，点击任意键退出"
+    raw_input()
+pass
