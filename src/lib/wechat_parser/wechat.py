4b13240155338307c62391289d0b4a698f6b83a5
姚泽源 <yaozeyuan93@gmail.com>
2017-01-27 15:39:02 +0800

    *   调整代码

--- src/lib/wechat_parser/wechat.py
+++ src/lib/wechat_parser/wechat.py
@@ -24,21 +24,24 @@ class WechatArticleParser(ParserTools):
 
     def get_article_info(self):
         data = {}
-        title =  self.dom.select('div#page-content h2.rich_media_title')
-        if len(title) == 0:
-            return []
-        data['title'] = title[0].get_text()
-        content_dom = self.dom.select('div#page-content div.rich_media_content')[0]
-        data['content'] = self.get_tag_content(content_dom)
+        try:
+            title =  self.dom.select('div#page-content h2.rich_media_title')
+            if len(title) == 0:
+                return []
+            data['title'] = title[0].get_text()
+            content_dom = self.dom.select('div#page-content div.rich_media_content')[0]
+            data['content'] = self.get_tag_content(content_dom)
 
-        data['updated_time'] = 0
-        data['voteup_count'] = 0
-        data['image_url'] = ''
-        data['comment_count'] = 0
-        data['author_id'] = 'meng-qing-xue-81'
-        data['author_name'] = '知乎助手'
-        data['author_headline'] = '微信魔改版'
-        data['author_avatar_url'] = 'https://pic4.zhimg.com/v2-38a89e42b40baa7d26d99cab9a451623_xl.jpg'
-        data['author_gender'] = '0'
+            data['updated_time'] = 0
+            data['voteup_count'] = 0
+            data['image_url'] = ''
+            data['comment_count'] = 0
+            data['author_id'] = 'meng-qing-xue-81'
+            data['author_name'] = '知乎助手'
+            data['author_headline'] = '微信魔改版'
+            data['author_avatar_url'] = 'https://pic4.zhimg.com/v2-38a89e42b40baa7d26d99cab9a451623_xl.jpg'
+            data['author_gender'] = '0'
+        except Exception:
+            return []
 
         return data
